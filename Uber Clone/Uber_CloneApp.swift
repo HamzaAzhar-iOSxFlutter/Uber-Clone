1b5469e41a23d7afa721296da53a80090b7e7cfa
Muhammad Hamza Azhar <hamzaazharcs@gmail.com>
2024-05-16 22:05:31 -0400

    Initial project base.

---
+++ Uber Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber Clone
+//
+//  Created by Hamza Azhar on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
